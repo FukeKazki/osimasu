7091603809a2f6cb27b6674c6814b8b8b0120cc9
FukeKazki <kazkichi0906@gmail.com>
2021-03-21 17:28:35 +0900

    Fix: 見ちゃだめ

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-	apiKey: "AIzaSyBbdOm62vde3wEvG5H5ziS8rINpROJVrfA",
-	authDomain: "osikore-dd167.firebaseapp.com",
-	projectId: "osikore-dd167",
-	storageBucket: "osikore-dd167.appspot.com",
-	messagingSenderId: "572740241278",
-	appId: "1:572740241278:web:cc66ad6cc573745c3ad52f",
-	measurementId: "G-7P684HZNGB"
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+	measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
